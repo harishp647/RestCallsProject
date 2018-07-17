3c60a2a8e22c6b6cd67e8e52076b386097aa033d
harishp647 <harishp1647@gmail.com>
2018-07-17 13:30:07 -0700

    Update& delete the data from AlienRepository.Java file
    added update & delete source code to the repository to AlienRepository branch

--- src/main/java/com/telusko/RestCallsProject/AlienRepository.java
+++ src/main/java/com/telusko/RestCallsProject/AlienRepository.java
@@ -90,7 +90,46 @@ public class AlienRepository {
 
 
 
-	
+	public Object updateAlienData(Alien a1) {
+		String sql = "update alien set name=? ,points=? where id=?" ;
+		 
+		try
+		{
+			PreparedStatement st = con.prepareStatement(sql);
+			
+			st.setString(1, a1.getName());
+			st.setInt(2,a1.getPoints());
+			st.setInt(3, a1.getId());
+			st.executeUpdate();
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return sql;
+	}
+
+
+
+	public Object deleteAlienData(int id) {
+String sql = "delete from alien where id=? ";
+		
+		try
+		{
+			PreparedStatement st = con.prepareStatement(sql);
+			
+			st.setInt(1,id);
+			
+			st.executeUpdate();
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		return sql;
+	}
 	
 	
 }
