40b25cdc75c12696f51eeb337b5b1b2039188e0b
harishp647 <harishp1647@gmail.com>
2018-07-17 13:21:20 -0700

    Upate and delete resource code were added to AlienResource Master branch
    New branch with update and delete requests

--- src/main/java/com/telusko/RestCallsProject/AlienResource.java
+++ src/main/java/com/telusko/RestCallsProject/AlienResource.java
@@ -3,8 +3,10 @@ package com.telusko.RestCallsProject;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -41,8 +43,28 @@ public class AlienResource {
 		 repo.createAlien(a1);
 		 
 	}
+	@Path("putaliensdata")
+	@PUT
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	
-
+	public Object updateAlien(Alien a1)
+	{
+		return  repo.updateAlienData(a1);
+		
+	}
+	@Path("deletealiensdata/{id}")
+	@DELETE
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Alien deleteAlien(@PathParam("id")int id)
+	{
+		Alien a = repo.getAlien(id);
+		if(a.getId()!=0) {
+		 repo.deleteAlienData(id);
+		}
+		return a;
+		
+	}
 	
 	
 	}
